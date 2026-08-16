1a2b7a48d1f055ef0efa8d6bdedb09e43cb1e4a9
amaal123-123 <elbaz055299@gmail.com>
2026-08-17 01:30:53 +0300

    Create data_type.cpp

---
+++ sheet1/data_type.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int a;
+    long long s;
+    char d;
+    float f;
+    double g;
+    cin>>a>>s>>d>>f>>g;
+    cout <<a<<"\n"<<s<<"\n"<<d<<"\n"<<f<<"\n"<<g<<endl;
+    return 0;
+}
