0680f02fb3a4b4fd6883fa94b9bebe9a36299160
amaal123-123 <elbaz055299@gmail.com>
2026-08-18 08:08:03 +0300

    Create condtion.cpp

---
+++ sheeet1/condtion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    long long a;
+    long long s;
+    cin>>a;
+    cin>>s;
+    cout <<a<<" + "<<s<<" = "<<a+s<<endl;
+    cout <<a<<" * "<<s<<" = "<<a*s<<endl;
+    cout <<a<<" - "<<s<<" = "<<a-s<<endl;
+ 
+ 
+    return 0;
+}
