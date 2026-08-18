09dfc6addcc7937934e6b578dc6d407a38511626
amaal123-123 <elbaz055299@gmail.com>
2026-08-18 08:15:31 +0300

    Create i.cpp

---
+++ sheet1/i.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+ 
+int main()
+{
+long long a,s;
+cin>>a>>s;
+if (a>=s){
+    cout <<"Yes";
+}
+else {
+    cout <<"No";
+}
+    return 0;
+}
