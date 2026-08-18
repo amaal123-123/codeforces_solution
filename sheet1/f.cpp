2d6046cd2ac4a160e75e4e68ca109de995bf64bc
amaal123-123 <elbaz055299@gmail.com>
2026-08-18 08:12:02 +0300

    Create f.cpp

---
+++ sheet1/f.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+ 
+int main()
+{
+ 
+long long  a;
+long long s;
+long long z;
+cin>>a>>s;
+z=(a%10)+(s%10);
+cout <<z;
+ 
+ 
+    return 0;
+}
