cdc04a909b23a860fb95f9f7adef232cff379515
amaal123-123 <elbaz055299@gmail.com>
2026-08-18 08:13:09 +0300

    Create g.cpp

---
+++ sheet1/g.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+ 
+int main()
+{
+    long long n;
+    cin>>n;
+    long long s=n*(n+1)/2;
+    cout<<s;
+ 
+    return 0;
+}
